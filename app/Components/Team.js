2a29a18d69aeee2912975149ae7a471ce50c09ba
Erik Ulberg <ulberge@gmail.com>
2016-02-09 19:03:58 -0800

    fix image 2

--- app/Components/Team.js
+++ app/Components/Team.js
@@ -4,6 +4,9 @@ import { Grid, Row, Col, Image } from 'react-bootstrap';
 
 import { className } from './Team.less';
 
+const erikImagePath = require('../../build/imgs/erik.png');
+const patrickImagePath = require('../../build/imgs/patrick.png');
+
 export default class extends Component {
   render() {
     return (
@@ -12,14 +15,14 @@ export default class extends Component {
           <Grid>
             <Row>
               <Col md={6}>
-                <Image width="100%" src="/../../imgs/erik.png" circle />
+                <Image width="100%" src={erikImagePath} circle />
                 <div className="text-center">
                   <h3>Erik Ulberg</h3>
                   <p>Doing everything yourself is a pain. We know development and we know excellent people who do everything else.  We can coordinate as much or as little as you want to make your web presence a breeze.</p>
                 </div>
               </Col>
               <Col md={6}>
-                <Image width="100%" src="/../../imgs/patrick.png" circle />
+                <Image width="100%" src={patrickImagePath} circle />
                 <div className="text-center">
                   <h3>Patrick Williams</h3>
                   <p>Excellent communication allows us to iterate efficiently with you as your ideas and business take shape.  You don''t need a perfect plan from the start.  We build flexibility into the product and scale as you grow.</p>
