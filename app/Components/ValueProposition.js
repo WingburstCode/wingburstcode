e41919af6a15466cfc57c39f0e578a72eeaa07eb
Erik Ulberg <ulberge@gmail.com>
2016-02-27 23:32:09 -0800

    text edit

--- app/Components/ValueProposition.js
+++ app/Components/ValueProposition.js
@@ -10,7 +10,7 @@ export default class extends Component {
   render() {
     return (
         <div className={classnames(className, 'text-center col-md-offset-2 col-md-8')}>
-          <h2>We Are Wingburst Code</h2>
+          <h2>Welcome to Wingburst Code</h2>
           <p>Design and development on web projects, large or small. Experts in Shopify and WordPress setup and customization. Features from $100 and websites from $1000.</p>
           <h3>We Know:</h3>
           <Image width="100%" src={expertImagePath} />
